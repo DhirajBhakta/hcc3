6be30b3ac73ad22a275b18d353dab9ffeb186fca
Ranganath <ranganath.pai@gmail.com>
2018-01-22 20:26:18 +0530

    Added prescription model and a centered header for main history page

---
+++ client/src/app/models/prescription.model.ts
@@ -0,0 +1,15 @@
+export class Prescription {
+    drugname: string;
+    quantity: string;
+    schedule: string;
+    comments: string;
+    constructor(drugname: string,
+                quantity: string,
+                schedule: string,
+                comments: string) {
+                    this.drugname = drugname;
+                    this.quantity = quantity;
+                    this.schedule = schedule;
+                    this.comments = comments;
+                }
+}
--- client/src/app/modules/patient/history/history.model.ts
+++ client/src/app/modules/patient/history/history.model.ts
@@ -1,8 +1,18 @@
+import { Prescription } from '../../../models/prescription.model';
+
 export class History {
     diagnosis: string;
     date: Date;
-    constructor( diagnosis: string, date: Date) {
+    name: string;
+    prescriptionList: Prescription[];
+
+    constructor( diagnosis: string,
+                date: Date,
+                name: string,
+                prescriptionList: Prescription[] ) {
         this.diagnosis = diagnosis;
         this.date = date;
+        this.name = name;
+        this.prescriptionList = prescriptionList;
     }
 }
