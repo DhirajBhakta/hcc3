98782effb7b8524306793a55dcd0ef18d0616662
DhirajBhakta <dhirajbhakta110@gmail.com>
2018-03-05 12:43:26 +0530

    MODEL UPDATE: removed 'store' from Drug models

--- server/api/models/Drug.py
+++ server/api/models/Drug.py
@@ -3,17 +3,23 @@ from django.db import models
 class Drug(models.Model):
     generic_name = models.CharField(max_length=255)
     trade_name = models.CharField(max_length=255)
-    
+
     class Meta:
-        unique_together = (('generic_name', 'trade_name'),)
+        unique_together = ('generic_name', 'trade_name')
 
 
 class Batch(models.Model):
-    name = models.CharField(max_length=255)
-    expiry_date = models.DateField()
+    #'batch' stands for 'batch_number' for consistency throughout the app
     drug = models.ForeignKey('Drug', on_delete=models.CASCADE, related_name='batches')
-
-class Store(models.Model):
-    rack = models.CharField(max_length=16)
+    batch = models.CharField(max_length=255)
+    rack = models.IntegerField()
     quantity = models.IntegerField()
-    batch = models.ForeignKey('Batch', on_delete=models.CASCADE, related_name='stores')
+    expiry_date = models.DateField()
+
+
+
+
+# class Store(models.Model):
+#     rack = models.CharField(max_length=16)
+#     quantity = models.IntegerField()
+#     batch = models.ForeignKey('Batch', on_delete=models.CASCADE, related_name='stores')
