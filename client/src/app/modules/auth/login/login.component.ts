64a92b342441844a59f0203e9f6466d6fd13d3e9
DhirajBhakta <dhirajbhakta110@gmail.com>
2018-03-08 01:06:43 +0530

    FRONT END: login component set up to handle JWT

--- client/src/app/modules/auth/login/login.component.ts
+++ client/src/app/modules/auth/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
-
-import { User } from '../user';
+import { User } from 'app/models/user.model';
 import { AuthService } from '../auth.service';
+import { CookieService } from 'angular2-cookie/core';
+
 
 @Component({
   selector: 'app-login',
@@ -11,8 +13,14 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  user: User;
-  constructor(private authService: AuthService) { this.user = new User('', ''); }
+  public username: String = null;
+  public password: String = null;
+  constructor(private authService: AuthService,
+    private router: Router,
+    private cookieService: CookieService) {
+    this.username = null;
+    this.pasword = null;
+  }
 
 
   ngOnInit() {
@@ -20,6 +28,13 @@ export class LoginComponent implements OnInit {
 
   onLogin() {
     console.log(this.user);
-    this.authService.login(this.user).subscribe(response => console.log(response));
+    if (this.username && this.password)
+      this.authService.login(this.username, this.password).subscribe(
+        (response) => {
+          console.log("User logged in, recieved response...", response.json());
+          console.log("Cookie holds this token:",this.cookieService.get('Bearer'));
+          this.router.navigateByUrl('/patient');
+        }
+      );
   }
 }
