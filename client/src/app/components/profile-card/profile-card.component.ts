126e7f8bf48cfeefbcde24dda6f6940cd02a089d
DhirajBhakta <dhirajbhakta110@gmail.com>
2018-03-10 19:20:39 +0530

    FRONTEND: profile-card handles brief mode and descriptive mode

--- client/src/app/components/profile-card/profile-card.component.ts
+++ client/src/app/components/profile-card/profile-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-profile-card',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-card.component.css']
 })
 export class ProfileCardComponent implements OnInit {
+ @Input() brief: boolean;
+ @Input() bundle: object;
 
   constructor() { }
 
   ngOnInit() {
+    console.log("BRIEF:",this.brief);
   }
 
 }
