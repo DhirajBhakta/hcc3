f49ce3547db786d4b60da3a9efa561d4454a522f
DhirajBhakta <dhirajbhakta110@gmail.com>
2018-03-11 23:37:13 +0530

    BACKEND:obtain_token now sends extra data multi_profile boolean which is true when an EMPLYOYEE logs in

--- server/api/JWTViews.py
+++ server/api/JWTViews.py
@@ -4,12 +4,12 @@ from rest_framework.response import Response
 class ObtainJWTView(ObtainJSONWebToken):
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
-        print("WOW:",response.data)
         if(response.data and response.data['token']):
             serializer = self.get_serializer(data=request.data)
             if(serializer.is_valid()):
                 user = serializer.object.get('user')
                 response.data['usergroup'] = user.groups.all()[0].name
+                response.data['multi_profile'] = (response.data['usergroup']=='PATIENT' and user.person.patient_type=='E')
         return response
 
 obtain_jwt_token = ObtainJWTView.as_view()
