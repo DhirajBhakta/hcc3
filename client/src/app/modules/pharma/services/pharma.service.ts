c18ac7bf5790a1da20c8bfb96d5a6b8425cf472c
DhirajBhakta <dhirajbhakta110@gmail.com>
2018-03-15 12:10:24 +0530

    FRONTEND: Pharma Stock-Update

---
+++ client/src/app/modules/pharma/services/pharma.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { URLSearchParams, RequestOptions, Headers } from '@angular/http';
+import { JWTHttpClient } from 'app/services/jwthttp.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+
+
+import { replaceKeys, prepareURL } from 'app/utils';
+import { environment } from 'environments/environment';
+
+@Injectable()
+export class PharmaService {
+
+  constructor(private http: JWTHttpClient) { }
+
+  getDrugNames() {
+    /*For autocomplete*/
+    let params = new URLSearchParams();
+    params.set('fields', JSON.stringify(['id', 'trade_name']));
+    return this.http.get(prepareURL(environment.server_base_url, 'drugs'), { params });
+  }
+
+  getInventory(){
+    return this.http.get(prepareURL(environment.server_base_url, 'batches'));
+  }
+
+  submitStockUpdate(stock_drugs){
+    const headers = new Headers();
+    headers.append('content-type', 'application/json');
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post(prepareURL(environment.server_base_url, 'batches'), JSON.stringify(stock_drugs),options);
+  }
+
+}
--- client/src/app/modules/pharma/stockupdate/stockupdate.component.ts
+++ client/src/app/modules/pharma/stockupdate/stockupdate.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { PharmaService } from '../services/pharma.service';
+import { AlertsService } from '@jaspero/ng-alerts';
+import  * as moment from 'moment';
 
 @Component({
   selector: 'app-stockupdate',
@@ -9,38 +11,54 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class StockupdateComponent implements OnInit {
 
-  stockUpdateForm;
+  stock_update_form;
+  drugs;
 
-  constructor(private fb: FormBuilder) {
-    this.stockUpdateForm = this.fb.group({
-      'stockDrugs': this.fb.array([])
+  constructor(private fb: FormBuilder, private phService: PharmaService, private _alerts: AlertsService) {
+    this.stock_update_form = this.fb.group({
+      'stock_drugs': this.fb.array([])
     });
   }
 
   ngOnInit() {
     this.addRow();
+    this.phService.getDrugNames().subscribe((resp) => this.drugs = resp.json());
   }
 
-
-  get stockDrugs(): FormArray {
-    return this.stockUpdateForm.get('stockDrugs') as FormArray;
+  get stock_drugs(): FormArray {
+    return this.stock_update_form.get('stock_drugs') as FormArray;
   };
 
+  onDrugSelect(item, row_id) {
+    this.stock_drugs.at(row_id).patchValue({ drug: item });
+  }
+
   addRow() {
-    this.stockDrugs.push(this.fb.group({
-      'genericName': ['', Validators.required],
+    this.stock_drugs.push(this.fb.group({
+      'drug': ['', Validators.required],
       'quantity': ['', [Validators.pattern(/^\d+$/), Validators.required]],
       'batch': ['', Validators.required],
-      'expiryDate': ['', [Validators.pattern(/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/), Validators.required]],
+      'expiry_date': ['', [Validators.pattern(/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/), Validators.required]],
       'rack': ['', Validators.required],
     }));
   }
 
   deleteRow(i) {
-    this.stockDrugs.removeAt(i);
+    this.stock_drugs.removeAt(i);
   }
 
-  onSubmit(){
-    console.log('Submitted');
+  onSubmit() {
+    let stock_drugs = this.stock_drugs.value.map((drug=>{
+                              drug.expiry_date = moment(drug.expiry_date,["DD-MM-YYYY","DD-MM-YY"]).format("YYYY-MM-DD");
+                              return drug;
+                            }));
+    this.phService.submitStockUpdate(stock_drugs).subscribe((resp) => {
+                              if (Math.floor(resp.status / 100) == 2) {
+                                this._alerts.create('success', 'Prescription successfully submitted');
+                                setInterval(() => window.location.reload(), 2000);
+                              };
+                            });
   }
+
+
 }
