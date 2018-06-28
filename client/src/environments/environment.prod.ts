6a45a56562eec81ffa148ecb17b33c5948ab2104
Ranganath Pai <ranganath.pai@gmail.com>
2018-06-28 13:30:11 +0530

    Fixed environment to real link

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  server_base_url: 'http://localhost:8000/api',
-  server_auth_url: 'http://localhost:8000',
+  server_base_url: 'http://hcc.nitk.ac.in/api',
+  server_auth_url: 'http://hcc.nitk.ac.in',
 };
